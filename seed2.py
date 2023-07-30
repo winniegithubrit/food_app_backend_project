d7c95d650acc5faec3574bd52962da62615e20fa
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-07-30 16:26:15 +0300

    Edited seed data

---
+++ seed2.py
@@ -0,0 +1,61 @@
+[{
+  "id": 1,
+  "first_name": "Bree",
+  "last_name": "Jeynes",
+  "email": "bjeynes0@spiegel.de",
+  "password": "pT5(`e?Z"
+}, {
+  "id": 2,
+  "first_name": "Paloma",
+  "last_name": "Spacey",
+  "email": "pspacey1@hexun.com",
+  "password": "qK8#,Z/\\<"
+}, {
+  "id": 3,
+  "first_name": "Merla",
+  "last_name": "Timbridge",
+  "email": "mtimbridge2@ameblo.jp",
+  "password": "bX0$)r*u"
+}, {
+  "id": 4,
+  "first_name": "Betsy",
+  "last_name": "Poznanski",
+  "email": "bpoznanski3@paypal.com",
+  "password": "oA0{KB66!"
+}, {
+  "id": 5,
+  "first_name": "Sibylla",
+  "last_name": "Kiley",
+  "email": "skiley4@scientificamerican.com",
+  "password": "eD7`e'S%"
+}, {
+  "id": 6,
+  "first_name": "Leontyne",
+  "last_name": "Fidge",
+  "email": "lfidge5@bing.com",
+  "password": "hL4$d=v&nLt"
+}, {
+  "id": 7,
+  "first_name": "Mario",
+  "last_name": "Gianettini",
+  "email": "mgianettini6@google.com",
+  "password": "kG6}knkI!t/@nG"
+}, {
+  "id": 8,
+  "first_name": "Brinn",
+  "last_name": "Aireton",
+  "email": "baireton7@google.co.jp",
+  "password": "jZ0}J`QfWzR"
+}, {
+  "id": 9,
+  "first_name": "Rachel",
+  "last_name": "Rawling",
+  "email": "rrawling8@guardian.co.uk",
+  "password": "tZ3+0W\\6m"
+}, {
+  "id": 10,
+  "first_name": "Hilary",
+  "last_name": "Jankovsky",
+  "email": "hjankovsky9@163.com",
+  "password": "iA7.vD{ulrl1{"
+}]
